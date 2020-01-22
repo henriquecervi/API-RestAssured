1d46db389a65ccca7348a485a8d4de35fb9b6474
henrique.cutri <henrique.cutri@SKAR1200658.rsinet.com.br>
2020-01-22 14:51:07 -0300

    Deserializando Api's.

---
+++ CursoRest/src/main/java/br/com/henrique/rest/TesteGit.java
@@ -0,0 +1,11 @@
+package br.com.henrique.rest;
+
+public class TesteGit {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Funciona Git.");
+	}
+
+}
---
+++ CursoRest/src/main/java/br/com/henrique/rest/UserJsonTest.java
@@ -0,0 +1,158 @@
+package br.com.henrique.rest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+public class UserJsonTest {
+	
+	@Test
+	public void deveVerificarPrimeiroNivel () {
+		
+		RestAssured.given()
+		.when()
+			.get("http://restapi.wcaquino.me/users/1")
+		.then()
+			.statusCode(200)
+			.body("id", Matchers.is(1))
+			.body("name", Matchers.containsString("Silva"))
+			.body("age", Matchers.greaterThan(18))
+		;
+		
+	}
+	
+	@Test
+	public void deveVerificarPrimeiroNivelOutrasFormas () {
+		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/users/1");
+		
+		//path
+		
+		Assert.assertEquals(new Integer(1), response.path("id"));
+		Assert.assertEquals(new Integer(1), response.path("%s" , "id"));
+		
+		//jsonpath
+		
+		JsonPath jpath = new JsonPath(response.asString());
+		Assert.assertEquals(1, jpath.getInt("id"));
+		
+		//from
+		
+		int id = JsonPath.from(response.asString()).getInt("id");
+		Assert.assertEquals(1, id);
+	}
+	
+	@Test
+	public void deveVerificarSegundoNivel () {
+		RestAssured.given()
+		.when()
+			.get("http://restapi.wcaquino.me/users/2")
+		.then()
+			.statusCode(200)
+			.body("id", Matchers.is(2))
+			.body("name", Matchers.containsString("Joaquina"))
+			.body("endereco.rua", Matchers.is("Rua dos bobos"))
+			.body("endereco.numero", Matchers.is(0))
+			;
+		
+	}
+	
+	@Test
+	public void deveVerificarComLista () {
+		RestAssured.given()
+		.when()
+			.get("http://restapi.wcaquino.me/users/3")
+		.then()
+		.statusCode(200)
+		.body("name", Matchers.containsString("Ana"))
+		.body("filhos", Matchers.hasSize(2))
+		.body("filhos[0].name", Matchers.is("Zezinho"))
+		.body("filhos[1].name", Matchers.is("Luizinho"))
+		.body("filhos.name", Matchers.hasItem("Luizinho"))
+		.body("filhos.name", Matchers.hasItems("Zezinho", "Luizinho"))
+		;
+		
+	}
+	
+	@Test
+	public void deveRetornarError() {
+		RestAssured.given()
+		.when()
+			.get("http://restapi.wcaquino.me/users/4")
+		.then()
+		.statusCode(404)
+		.body("error", Matchers.is("Usuário inexistente"))
+		;
+		
+	}
+	
+	@Test
+	public void deveVerificarListaRaiz () {
+		RestAssured.given()
+		.when()
+			.get("http://restapi.wcaquino.me/users")
+		.then()
+		.statusCode(200)
+		.body("$", Matchers.hasSize(3))
+		.body("name", Matchers.hasItems("João da Silva", "Maria Joaquina", "Ana Júlia"))
+		.body("age[1]", Matchers.is(25))
+		.body("filhos.name", Matchers.hasItem(Arrays.asList("Zezinho", "Luizinho")))
+		.body("salary", Matchers.contains(1234.5678f, 2500, null))
+		;
+		
+	}
+	
+	@Test
+	public void devoFazerVerificoesAvancadas() {
+		RestAssured.given()
+		.when()
+			.get("http://restapi.wcaquino.me/users")
+		.then()
+		.statusCode(200)
+		.body("$", Matchers.hasSize(3))
+		.body("age.findAll{it <= 25}.size()", Matchers.is(2)) // o IT verifica o age, ele pega a primeira referencia de parametro.
+		.body("age.findAll{it <= 25 && it > 20}.size", Matchers.is(1))
+		.body("findAll{it.age <= 25 && it.age > 20}.name", Matchers.hasItem("Maria Joaquina"))
+		.body("findAll{it.age <= 25}[0].name", Matchers.is("Maria Joaquina")) // começa a verificar todos os itens e depois filtra por idade e [0] pega o primeiro item da lista.
+		.body("findAll{it.age <= 25}[-1].name", Matchers.is("Ana Júlia")) // verifica o último item da lista.
+		.body("find{it.age <= 25}.name", Matchers.is("Maria Joaquina"))
+		.body("findAll{it.name.contains('n')}.name", Matchers.hasItems("Maria Joaquina", "Ana Júlia"))
+		.body("findAll{it.name.length() > 10}.name", Matchers.hasItems("Maria Joaquina", "João da Silva"))
+		.body("name.collect{it.toUpperCase()}", Matchers.hasItem("MARIA JOAQUINA"))
+		.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}", Matchers.hasItem("MARIA JOAQUINA"))
+		.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()", 
+				Matchers.anyOf(Matchers.arrayContaining("MARIA JOAQUINA"), Matchers.arrayWithSize(1))) // verificar, precisa converter para string
+		.body("age.collect{it * 2}", Matchers.hasItems(60, 50, 40))
+		.body("id.max()", Matchers.is(3))
+		.body("salary.min()", Matchers.is(1234.5678f))
+		.body("salary.findAll{it != null}.sum()", Matchers.is(Matchers.closeTo(3734.5678f, 0.001)))
+		.body("salary.findAll{it != null}.sum()", Matchers.allOf(Matchers.greaterThan(3000d), Matchers.lessThan(5000d)))
+		;
+		
+	}
+	
+	@Test
+	public void deveUnirJsonPathComJava () {
+		ArrayList<String> names =
+				RestAssured.given()
+				.when()
+					.get("http://restapi.wcaquino.me/users")
+				.then()
+				.statusCode(200)
+				.extract().path("name.findAll{it.startsWith('Maria')}")
+				;
+		
+		Assert.assertEquals(1, names.size());
+		Assert.assertTrue(names.get(0).equalsIgnoreCase("mArIa JoaqUinA"));
+		Assert.assertEquals(names.get(0).toUpperCase(), "Maria Joaquina".toUpperCase());				
+	}
+	
+
+}
