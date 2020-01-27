b9d9830813611124ff93b4ca207832cb6a27eadb
henrique.cutri <henrique.cutri@SKAR1200658.rsinet.com.br>
2020-01-27 13:46:49 -0300

    Validando Schema Json!

--- CursoRest/src/main/java/br/com/henrique/rest/SchemaTest.java
+++ CursoRest/src/main/java/br/com/henrique/rest/SchemaTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.xml.sax.SAXParseException;
 
 import io.restassured.matcher.RestAssuredMatchers;
+import io.restassured.module.jsv.JsonSchemaValidator;
 
 
 public class SchemaTest {
@@ -35,6 +36,19 @@ public class SchemaTest {
 			.body(RestAssuredMatchers.matchesXsdInClasspath("users.xsd"))
 		;
 		
+	}
+	
+	@Test
+	public void deveValidarSchemaJson() {
+		given()
+			.log().all()
+		.when()
+			.get("http://restapi.wcaquino.me/users")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("users.json"))
+		;
 	}
 
 }
